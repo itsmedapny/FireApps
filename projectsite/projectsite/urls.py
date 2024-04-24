d383d01e0bfe97daaad6a23e23b4d66a542c57f4
iAmRangz <superango2012@gmail.com>
2024-04-24 13:34:17 +0800

    dynamic multiline chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
 from fire import views
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
 ]
