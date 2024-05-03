c85fde666ceab6556567cef1ade476e1a741d104
itsmedapny <madaphneeevediente@gmail.com>
2024-05-03 16:49:07 +0800

    Finalize

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
-from fire import views
 
+from fire.views import HomePageView
+from fire import views 
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
     path('multiBarChart/', multipleBarbySeverity, name='chart'),
+    path('stations', views.map_station, name='map-station'),
 ]
